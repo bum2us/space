fc148c47c50521eb8a9561ea6677b95198d0aee9
bum2us <qka342@gmail.com>
2022-11-29 15:37:28 +0900

    chat
    - 채팅과 상품 데이터 연동

--- src/main/java/com/space/infra/modules/chat/Chat.java
+++ src/main/java/com/space/infra/modules/chat/Chat.java
@@ -13,6 +13,14 @@ public class Chat {
 	private String mmNickName;
 	private String upPath;
 	private String upUuidName;
+
+	//채팅 상품 정보 추출을 위한 필드
+	private Integer pdSeq;
+	private Integer pdState;
+	private String pdTitle;
+	private String pdPrice;
+	private String pdPath;
+	private String pdUuidName;
 	
 	public Integer getChatSeq() {
 		return chatSeq;
@@ -67,6 +75,42 @@ public class Chat {
 	}
 	public void setUpUuidName(String upUuidName) {
 		this.upUuidName = upUuidName;
+	}
+	public Integer getPdSeq() {
+		return pdSeq;
+	}
+	public void setPdSeq(Integer pdSeq) {
+		this.pdSeq = pdSeq;
+	}
+	public Integer getPdState() {
+		return pdState;
+	}
+	public void setPdState(Integer pdState) {
+		this.pdState = pdState;
+	}
+	public String getPdTitle() {
+		return pdTitle;
+	}
+	public void setPdTitle(String pdTitle) {
+		this.pdTitle = pdTitle;
+	}
+	public String getPdPrice() {
+		return pdPrice;
+	}
+	public void setPdPrice(String pdPrice) {
+		this.pdPrice = pdPrice;
+	}
+	public String getPdPath() {
+		return pdPath;
+	}
+	public void setPdPath(String pdPath) {
+		this.pdPath = pdPath;
+	}
+	public String getPdUuidName() {
+		return pdUuidName;
+	}
+	public void setPdUuidName(String pdUuidName) {
+		this.pdUuidName = pdUuidName;
 	}
 	
 	
