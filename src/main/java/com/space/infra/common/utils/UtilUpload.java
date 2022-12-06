f7d93deca9134705b7ab5bfa8d95170bde56e018
bum2us <qka342@gmail.com>
2022-12-06 11:13:45 +0900

    업로드 경로 변경

--- src/main/java/com/space/infra/common/utils/UtilUpload.java
+++ src/main/java/com/space/infra/common/utils/UtilUpload.java
@@ -21,7 +21,8 @@ public class UtilUpload {
 		String pathDate = nowString.substring(0,4) + "/" + nowString.substring(5,7) + "/" + nowString.substring(8,10);
 		//String path = "D://factory/ws_sts_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
 		//EC2 서버용
-		String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		//String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		String path = "/01_DEV/wsEzen/space/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
 		String pathForView = "/resources/uploaded/" + pathModule + "/" + pathDate + "/";
 		
 		createPath(path); //위에서 정의한 경로를 생성해주는 메소드
@@ -46,7 +47,8 @@ public class UtilUpload {
 		String pathModule = className;
 		String nowString = UtilDateTime.nowString();
 		String pathDate = nowString.substring(0,4) + "/" + nowString.substring(5,7) + "/" + nowString.substring(8,10);
-		String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		String path = "/01_DEV/wsEzen/space/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		//String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
 		String pathForView = "/resources/uploaded/" + pathModule + "/" + pathDate + "/";
 		
 		createPath(path); //위에서 정의한 경로를 생성해주는 메소드
@@ -71,7 +73,8 @@ public class UtilUpload {
 		String pathModule = className; 
 		String nowString = UtilDateTime.nowString(); 
 		String pathDate = nowString.substring(0,4) + "/" + nowString.substring(5,7) + "/" + nowString.substring(8,10); 
-		String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		//String path = "C://factory/WS_STS_4151/space/src/main/webapp/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
+		String path = "/01_DEV/wsEzen/space/resources/uploaded" + "/" + pathModule + "/" + pathDate + "/";
 		//EC2 서버용
 		String pathForView = "/resources/uploaded/" + pathModule + "/" + pathDate + "/";
 		 
